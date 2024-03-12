773da872442d05ab833125d12e93b4ea11547ce4
Kuba Smide <smide223@gmail.com>
2024-03-12 13:00:07 +0100

    i created a local package with setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = "Knowledgenow",
+    version = "0.0.0",
+    author = "Smide Kuba",
+    author_email = "smide223@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
