d2eff6a01b47ce5d9c2456884dd49928813dd44f
Kuba Smide <smide223@gmail.com>
2024-03-18 11:53:42 +0100

    First commit

--- app.py
+++ app.py
@@ -1,52 +1,77 @@
-from flask import Flask, render_template, jsonify, request
-from src.helper import data_embedding
-from langchain_pinecone import PineconeVectorStore
-from langchain.prompts import PromptTemplate
+#import Lbraries
+import streamlit as st 
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain_community.embeddings import OpenAIEmbeddings
+import pinecone
+from langchain.llms import OpenAI
+from langchain_community.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
-from dotenv import load_dotenv
-from src.prompt import *
+from langchain.prompts import PromptTemplate
 import os
-from langchain_openai.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.tools.retriever import create_retriever_tool
+from langchain_pinecone import PineconeVectorStore
+import pickle 
+import os
+from dotenv import load_dotenv 
+import chardet
 
-app = Flask(__name__)
 
 load_dotenv()
 
-openai_api_key = os.environ.get("OPENAI_API_KEY")
-pinecone_api_key = os.environ.get("PINECONE_API_KEY")
-
-embeddings = data_embedding()
-index_name = "knowledgenow"
-vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
-
-prompt = PromptTemplate(template=template, input_variables=["context", "question"])
-
-chain_type_kwargs = {"prompt": prompt}
-
-llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-4")
-
-retriever = vectorstore.as_retriever(search_kwargs={'k': 2})
-
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=retriever,
-    chain_type_kwargs=chain_type_kwargs
-)
-
-@app.route("/")
-def index():
-    return render_template("chat.html")
+#add your api_keys
+openai_api_key = os.getenv("OPENAI_API_KEY")
+os.getenv("PINECONE_API_KEY")
 
 
-@app.route("/get", methods=['GET',"POST"])
-def chat():
-    msg = request.form["msg"]
-    input = msg 
-    print(input)
-    result=qa({"query": input})
-    print("Response:", result["result"])
-    return str(result["result"])
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#Sidebar contents
+with st.sidebar:
+    st.title("Fastrise Chat App")
+    st.markdown('''
+                ## About 
+                This app is an LLM-powered chatbot built using:
+                
+                -[Fastrise Knowledge base](fasterise.com)
+                
+                -[Langchain](https://python.langchain.com)
+                
+                -[OpenAI](https://platform.openai.com/docs/models)
+                ''')
+    st.write("Made with love by KUBA")
+
+
+def main():
+    st.header("KNOWLEDGENOW")
+
+    #upload a PDF file 
+    text_file = st.file_uploader("upload your file", type=["txt"])
+    raw_data = text_file.read(10000)  # Read the first 10000 bytes to guess encoding
+    encoding = chardet.detect(raw_data)['encoding']
+    #st.write(text_file.name)
+    
+    if text_file is not None:
+        # Ensure the file pointer is at the start
+        text_file.seek(0)
+
+        # Read the file line by line into a list
+        file_lines = [line.decode(encoding).strip() for line in text_file]
+
+         
+   # Load the text document using TextLoader
+    text_reader = UnstructuredFileLoader(file_lines)
+    text_document = text_reader.load()
+    
+
+
+
+    st.write(text_document)
+
+    
+    
+
+
+if __name__ =="__main__":
+    main()
