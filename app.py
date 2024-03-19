634d09418305abd5c3a015c01db91cdd65a63bda
Kuba Smide <smide223@gmail.com>
2024-03-19 13:21:06 +0100

    updated the code structure

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain_community.embeddings import OpenAIEmbeddings
 import pinecone
-from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate
 import os
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.tools.retriever import create_retriever_tool
 from langchain_pinecone import PineconeVectorStore
-import pickle 
 import os
 from dotenv import load_dotenv 
-import chardet
-
-
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParserOutputParser
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 load_dotenv()
 
 #add your api_keys
@@ -26,6 +25,9 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 os.getenv("PINECONE_API_KEY")
 
 
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
 #Sidebar contents
 with st.sidebar:
@@ -43,35 +45,62 @@ with st.sidebar:
     st.write("Made with love by KUBA")
 
 
-def main():
-    st.header("KNOWLEDGENOW")
 
-    #upload a PDF file 
-    text_file = st.file_uploader("upload your file", type=["txt"])
-    raw_data = text_file.read(10000)  # Read the first 10000 bytes to guess encoding
-    encoding = chardet.detect(raw_data)['encoding']
-    #st.write(text_file.name)
+
+#get  response 
+def get_response(query, chat_history):
+    template = """
+ Your role is to use the global context to answer any question from users. 
+
+
+ {chat_history}
+
+ global_context: {global_context}
+
+Begin!
+
+ Question: {input}
+
+ Thoughts: {agent_scratchpad}
+ 
+ """
+
+
+prompt = ChatPromptTemplate.from_template(template=template, MessagePlaceholder=["(agent_scratchpad)"])
+
+
+
+llm  = ChatOpenAI(
+    openai_api_key=openai_api_key, 
+    model="gpt-4-turbo-preview", 
+    temperature= 0.0,
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()]
+
+    )
+
+
+
+#conversations
+for message in st.session.chat_history:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
     
-    if text_file is not None:
-        # Ensure the file pointer is at the start
-        text_file.seek(0)
 
-        # Read the file line by line into a list
-        file_lines = [line.decode(encoding).strip() for line in text_file]
+    else:
+        with st.chat_message("AI"):
+            st.markdown(message.content)
 
-         
-   # Load the text document using TextLoader
-    text_reader = UnstructuredFileLoader(file_lines)
-    text_document = text_reader.load()
-    
+#user input
+user_query = st.chat_input("Your message")
+if user_query is not None and user_query !="":
+    st.session_state.chat_history.append(HumanMessage(user_query))
 
 
+    with st.chat_message("Human"):
+        st.markdown(user_query)
 
-    st.write(text_document)
-
-    
-    
-
-
-if __name__ =="__main__":
-    main()
+    with st.chat_message("AI"):
+        ai_response = "i don't know"
+        st.markdown(ai_response)
